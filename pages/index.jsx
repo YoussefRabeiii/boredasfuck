a59c5f59df9f22dc02a33a98f0cecfb1ed81e32e
Youssef Rabei <youssefrabei2003@gmail.com>
2022-09-28 19:25:27 +0200

    🚑 Fixing a link

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,12 +14,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="/">Bored As Fuck !</a>
+          Welcome to <a href="#">Bored As Fuck !</a>
         </h1>
 
         <p className={styles.description}>
           where I will be <code className={styles.code}>coding</code> some text
-          animations and some other stupid little stuff <code className={styles.code}>no libraries allowed</code> <p style={{ fontSize:"1rem" }}>(cause I hate myself )</p>
+          animations and some other stupid little stuff{" "}
+          <code className={styles.code}>no libraries allowed</code>{" "}
+          <p style={{ fontSize: "1rem" }}>(cause I hate myself )</p>
         </p>
 
         <Liquid
